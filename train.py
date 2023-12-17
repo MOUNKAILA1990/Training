66563bbe58af52d7d77bee8ae6ab569572356780
Hassane Mounkaila <mounkailah2040@gmail.com>
2023-12-17 02:42:37 +0100

    Ajout de LSTM model

--- train.py
+++ train.py
@@ -1,1 +1,6 @@
-print("Hello")
+import tensorflow as tensorflow
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Embedding, Bidirectional, LSTM
+
+num_words = 10000 # nombre de mots à utiliser
+maxlen = 100 # longueur maximale de chaque avis
